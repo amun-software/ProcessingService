dffa1f9e0b5fb5295886842e52ae308f3676be53
dwalin93 <p.glahe@gmx.de>
2018-01-11 12:26:56 +0100

    Update AmunSentinelProcessing.R

--- R/AmunSentinelProcessing.R
+++ R/AmunSentinelProcessing.R
@@ -61,7 +61,7 @@ NDVI = function(x,y){
 
 #return result
 NDVI_Result = function(x,y){
-  output = raster::overlay(raster::raster(x), raster::raster(y), fun = NDVI)
+  output = raster::overlay(raster::raster(base::normalizePath(x)), raster::raster(base::normalizePath(y)), fun = NDVI)
   graphics::par(bg=NA)
   raster::plot(output,axes=FALSE, box=FALSE, legend=FALSE)
   
