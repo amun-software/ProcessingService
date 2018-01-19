70307ec3e83d44ed156c61d41fd9260fd76fe8ad
dwalin93 <p.glahe@gmx.de>
2018-01-19 09:10:59 +0100

    Update AmunSentinelProcessing.R

--- R/AmunSentinelProcessing.R
+++ R/AmunSentinelProcessing.R
@@ -21,7 +21,7 @@ TCI = function(channel1,channel2,channel3){
   RGB_brick= raster::brick(r,g,b)
   RGB_brick[RGB_brick<=0] = NA
   graphics::par(bg=NA,mar=c(0,0,0,0),oma=c(0,0,0,0))
-  raster::plotRGB(RGB_brick,r=1,g=2,b=3,stretch="lin",bgalpha=0)
+  raster::plotRGB(RGB_brick,r=1,g=2,b=3,stretch="hist",bgalpha=0)
 }
 
 #' Delivers the TCI image as grayscale
