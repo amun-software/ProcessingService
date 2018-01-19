6fc765ae3ff940d0f174a70319faad416c305707
dwalin93 <p.glahe@gmx.de>
2018-01-19 09:01:44 +0100

    Update AmunSentinelProcessing.R

--- R/AmunSentinelProcessing.R
+++ R/AmunSentinelProcessing.R
@@ -9,60 +9,6 @@ test = function(number){
   }
 
 ##########################Kanal-Mapping########################################
-TCI = function(channel1,channel2,channel3){
-  r = raster::raster(channel1)
-  g = raster::raster(channel2)
-  b = raster::raster(channel3)
-  RGB_brick= raster::brick(r,g,b)
-  RGB_brick[RGB_brick<=0] = NA
-  graphics::par(bg=NA,mar=c(0,0,0,0),oma=c(0,0,0,0))
-  raster::plotRGB(RGB_brick,r=1,g=2,b=3,stretch="hist",bgalpha=0)
-}
-
-
-TCI_grayscale = function(img){
-  rasterIMG = raster::raster(img)
-  grayscale = gray.colors(100, # number of colors
-                          start = 0.0, #black
-                          end = 1.0, # white
-                          gamma=2.2, #correction from linear to nonlinear (conversion from camera to human eye)
-                          alpha = NULL)# transparency
-  graphics::par(bg=NA,mar=c(0,0,0,0),oma=c(0,0,0,0))
-  raster::image(rasterIMG,col=grayscale,axes=FALSE,legend=FALSE, frame=FALSE)
-
-}
-
-#var = TCI(Band1,Band2,Band3)
-#TCI_grayscale(Band1)
-
-##########################Contrast Adjustments#################################################
-
-Contrast = function(band,min,max){
-  rasterBand = raster::raster(band)
-  newMin = min
-  newMax = max
-  rasterBand[rasterBand<=newMin] = NA
-  rasterBand[rasterBand>=newMax] = NA
-  minValue = raster::minValue(rasterBand)
-  maxValue = raster::maxValue(rasterBand)
-  scale = scaling(rasterBand,minValue,maxValue)
-  
-}
-
-scaling = function (rasterBand,minValue,maxValue){
-  ((rasterBand-minValue)*255 / (maxValue -minValue) + 0)
-}
-
-ContrastRGB = function(red,green,blue,rmin,rmax,gmin,gmax,bmin,bmax){
-  r = Contrast(red,rmin,rmax)
-  g = Contrast(green,gmin,gmax)
-  b = Contrast(blue,bmin,bmax)
-  graphics::par(bg=NA,mar=c(0,0,0,0),oma=c(0,0,0,0))
-  raster::plotRGB(RGB_brick,r=1,g=2,b=3,stretch="lin")
-}
-
-
-##########################Kanal-Mapping########################################
 #' TCI Function
 #' @param channel1 First Channel of the image
 #' @param channel2 Second Channel of the image
@@ -73,7 +19,9 @@ TCI = function(channel1,channel2,channel3){
   g = raster::raster(channel2)
   b = raster::raster(channel3)
   RGB_brick= raster::brick(r,g,b)
-  raster::plotRGB(RGB_brick,r=1,g=2,b=3,stretch="lin",colNA=FALSE,bgalpha=0)
+  RGB_brick[RGB_brick<=0] = NA
+  graphics::par(bg=NA,mar=c(0,0,0,0),oma=c(0,0,0,0))
+  raster::plotRGB(RGB_brick,r=1,g=2,b=3,stretch="hist",bgalpha=0)
 }
 
 #' Delivers the TCI image as grayscale
